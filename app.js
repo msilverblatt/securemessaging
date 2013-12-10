79fe62e6860cf9d62c1c39d0144472fa81dd5c9f
Tim Alander <tim@red>
2013-12-10 15:26:18 -0500

    basic routes

--- app.js
+++ app.js
@@ -4,10 +4,22 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
+
+// Routes
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
+app.get('/send', function(req, res) {
+  res.send('Send page');
+});
+
+app.get('/receive', function(req, res) {
+  res.send('Receive page');
+});
+
+
+// Server configuration
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
