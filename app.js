9687944e3d242c371c48ef55898f7be9cf79d8cb
timalander <tim@red>
2013-12-10 15:47:34 -0500

    Added mongo

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 var express = require("express");
 var app = express();
+var mongo = require('mongodb');
 
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/mydb';
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.sendfile('public/index.html');
 });
 
 app.get('/send', function(req, res) {
-  res.send('Send page');
+  res.sendfile('public/send.html');
 });
 
 app.get('/receive', function(req, res) {
-  res.send('Receive page');
+  res.sendfile('public/receive.html');
+});
+
+app.get('/register', function(req, res) {
+  res.sendfile('public/register.html');
 });
 
 
